e5013ae4cf5a1b6e9c95d4e96f6b64a3891728ef
George Padmore Yeboah <34770919+GeorgePadmore@users.noreply.github.com>
2019-05-01 19:11:30 +0000

    Update booking.php

--- booking.php
+++ booking.php
@@ -1,9 +1,9 @@
 <?php
 
-//<full name> <email>.
+//<George Padmore Yeboah> <padmorey@gmail.com>.
 interface BookingStructure
 {
-    private function bookASlot($from, $to);
+    public function bookASlot($from, $to);
 }
 
 
@@ -13,29 +13,83 @@ class Booking implements BookingStructure
         ['from'=>'8:00', 'to'=>'9:30']
        
     ];
-    public function _construct($openingTime, $closingTime)
+    private $booking_openingTime, $booking_closingTime;
+
+    private $minMinutes = 30; //30 minutes;
+    private $maxMinutes = 120; //2 hours (converted to minutes.) NB: 1hr = 60 minutes. 
+
+
+    public function __construct($openingTime, $closingTime)
     {
         //add code here
+        $this->booking_openingTime = $openingTime; 
+        $this->booking_closingTime = $closingTime; 
     }
     
     public function getAllBookings()
     {
         // add code here
+        return $this->bookedSlots;
     }
     
-    public function bookASlot()
+    public function bookASlot($from, $to)
     {
+        
         //add code here
+        $bookedSlotsFrom = "";
+        $bookedSlotsTo = "";
+        $message = "";
+        $totalTimeInMinutes = 0;
+
+        $from = preg_replace("([^0-9:])", "", $from);//Validate to accept only allowed characters and inputs
+        $to = preg_replace("([^0-9:])", "", $to);//Validate to accept only allowed characters and inputs
+
+        #get the number of minutes this booking is.
+        $totalTimeInMinutes = abs((new \DateTime($from))->getTimestamp() - (new \DateTime($to))->getTimestamp()) / 60;
+
+        try {
+ 
+            #check new booking against the minimum and maximum time.
+            if ($totalTimeInMinutes < $this->minMinutes){
+                throw new Exception("Sorry you can't book less than a 30 min slot<br>");
+            }
+    
+            //if booking time falls within the accepted 2 hours.
+            if ($totalTimeInMinutes > $this->maxMinutes){
+                throw new Exception("Sorry you can't book above a 2 hour slot in<br>");
+            } 
+
+            
+            foreach ($this->bookedSlots as $key => $value) {
+            
+                $bookedSlotsFrom = $value["from"];
+                $bookedSlotsTo = $value["to"];
+
+                //check if booking slot is available for this new booking time.
+                if (($value["from"] <= $from) && ($to <= $value["to"])) {
+                    throw new Exception("Sorry there is a meeting from $bookedSlotsFrom to $bookedSlotsTo <br>");
+                }else{
+                    $newSlot = ['from'=>$from, 'to'=>$to];
+                    array_push($this->bookedSlots, $newSlot);//can now add to booked slots.
+                }
+
+            }
+        }
+        catch(Exception $e) {        
+            echo 'Uncaught exception: Exception ' .$e->getMessage();
+        }
+
+        return true;
     }
     
     public function getOpeningTime()
     {
-        // add code here
+        return $this->booking_openingTime;
     }
     
     public function getClosingTime()
     {
-        // add code here
+        return $this->booking_closingTime;
     }
 }
 
